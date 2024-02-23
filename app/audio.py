52275af98cfe39c2846e757ff9ae40793cf330f3
SadeghKrmi <sadegh.krmi@gmail.com>
2024-02-23 17:04:29 +0000

    updating the app

--- app/audio.py
+++ app/audio.py
@@ -1,27 +1,31 @@
+import os
+import soxr
 import logging
 import soundfile
 import traceback
 
-def read_audio(file):
+def read_audio(file, target_sr=None):
     logging.debug("Reading audio with soundfile: %s" % file)
     try:
         logging.debug(f"Reading audio: {file.name if hasattr(file, 'name') else file}")
         audio, sr = soundfile.read(file, dtype="float32", always_2d=True)
         logging.info(f"Read audio {file}: samplerate={sr} shape={audio.shape}")
         assert audio.shape[0] > 0, "Audio is empty"
-        return audio
+        if target_sr is not None and sr != target_sr:
+            logging.info(f"Samplerate {sr} doesn't match target {target_sr}, resampling with SoXR")
+            audio = soxr.resample(audio, sr, target_sr, "VHQ")
+            sr = target_sr
+        return audio, sr
     except:
         logging.error("Failed to read with soundfile:\n" + traceback.format_exc())
 
 
-def save_audio(file, audio, format="wav"):
+def save_audio(path, audio, sr, format="PCM_24"):
     try:
-        soundfile.write(file, audio.transpose(0, 1).numpy(), sr, format)
+        os.makedirs(path)
+        for stem, source in audio.items():
+            file = path + "/" + stem + ".wav"
+            soundfile.write(file, source.transpose(0, 1).numpy(), sr, format)
     except soundfile.LibsndfileError:
-        logging.error(f"Failed to write file {file}:\n" + traceback.format_exc())
+        logging.error(f"Failed to write to the output {path}:\n" + traceback.format_exc())
         return
-    logging.info(f"Saved audio {file}: shape={audio.shape}")
-
-
-def download_mode(model):
-    pass
--- app/separator.py
+++ app/separator.py
@@ -1,43 +1,85 @@
 import os
-import glob
+import sys
+import torch
 from pathlib import Path
-import demucs.api as deap
+import demucs.api as deapi
+import demucs.apply as deapply
 import typing as tp
-import audio
+import audio as ao
 import logging
-
+import time
 
 repo = Path("/app/models/")
 audio_file_wav = "/app/audio-in/f8JTfLsyIHg_ffmpeg_noisy.wav"
 audio_file_mp3 = "/app/audio-in/audio_example.mp3"
+audio_out_path = "/app/audio-out/"
+
+
+def parse_arguments(args):
+    parsed_args = {}
+    i = 1
+    while i < len(args):
+        if args[i].startswith("--") and i + 1 < len(args):
+            key = args[i][2:]
+            value = args[i + 1]
+            parsed_args[key] = value
+            i += 2
+        else:
+            print(f"Ignoring argument: {args[i]}")
+            i += 1
+    return parsed_args
+
+    
 
 
 class Separator:
     def __init__(self, repo: tp.Optional[tp.Callable[[str], None]], model: str = "htdemucs"):
-        self.separator = deap.Separator(model=model, progress=False)
+        self.separator = deapi.Separator(model=model, repo=repo, progress=False)
         self.model = model
+        self.repo = repo
         
-    def separate(self, file):
-        filename = os.path.basename(file)
-        logging.info("Start separating audio: %s" % filename)
-        origin, separated = self.separator.separate_audio_file(file)
-
-
-
-    # def download_model(self, model):
-    #     remote_urls = {}
-    #     new_models = deap.list_models()["bag"]
-    #     for sig, filepath in new_models.items():
-    #         remote_urls[sig] = str(filepath)
-            
-    #     if isinstance(remote_urls[model], list):
-    #         pass
-        # if model in self.downloaded_models:
-        #     req = urllib.request.Request(url, headers={"User-Agent": "torch.hub"})
-        #     u = urllib.request.urlopen(req)
-        #     return
+        if not isinstance(self.separator.model, deapply.BagOfModels):
+            self.default_segment = self.separator.model.segment
+        else:
+            self.default_segment = min(i.segment for i in self.separator.model.models)  # type: ignore
+            if hasattr(self.separator.model, "segment"):
+                self.default_segment = min(self.default_segment, self.separator.model.segment)
+        self.default_segment = max(self.default_segment, 0.1)
+        self.sources = self.separator.model.sources
         
+    def separate_from_audio(self, audio_file):
+        wav, sampling_rate_of_audio = ao.read_audio(audio_file)
+        self.filename = os.path.basename(audio_file)
+        self.filepath = os.path.abspath(audio_file)
+        self.output_path = "/app/audio-out/" + self.filename.split(".")[0]
+        self.time_hists = []
         
+        logging.info(f"Start separating {self.filename} audio")
+        
+        wav_torch = torch.from_numpy(wav).clone().transpose(0, 1)
+        src_channels = wav_torch.shape[0]
+        logging.info("Running separation...")
+        self.time_hists.append((time.time(), 0))
+        if src_channels != self.separator.model.audio_channels:
+            out = {stem: torch.zeros(1, wav_torch.shape[1], dtype=torch.float32) for stem in self.sources}
+            self.in_length = src_channels
+            self.out_length = 0
+            for i in range(src_channels):
+                self.out_length += 1
+                for stem, tensor in self.separator.separate_tensor(
+                    wav_torch[i, :].repeat(self.separator.model.audio_channels, 1)
+                )[1].items():
+                    out[stem][i, :] = tensor.sum(dim=0) / tensor.shape[0]
+        else:
+            self.in_length = 1
+            self.out_length = 0
+            out = self.separator.separate_tensor(wav_torch)[1]
+        ao.save_audio(self.output_path, out, sr=sampling_rate_of_audio)
+        return
 
-sep = Separator(model="htdemucs", repo =None)
-origin, separated = sep.separate(audio_file_wav)
+
+if __name__ == "__main__":
+    options = parse_arguments(sys.argv)
+    print(options)
+    # sep = Separator(model="htdemucs", repo=None)
+    # sep.separate_from_audio(audio_file_wav)
